c5505ef1f0efce4382051da6924a2a5d376e26fe
Rushang Gajjal <rushang101@gmail.com>
2019-02-16 05:40:16 +0530

    Update

--- cipher/source/index.php
+++ cipher/source/index.php
@@ -1,34 +1,64 @@
-function xor($string, $key){
-    for($i = 0; $i < strlen($string); $i++) 
-        $string[$i] = ($string[$i] ^ $key[$i % strlen($key)]);
-    return $string;
-}
-$success = 0;
-$cipher = "WebTF{}";
-$ascii = $_GET['ascii'] == iconv('UTF-8','ASCII',$cipher);
-$hex = $_GET['hex'] == bin2hex($cipher);
-$rot13 = $_GET['rot13'] == str_rot13($cipher);
-$base64 = $_GET['base64'] == base64_encode($cipher);
-$md5 = $_GET['md5'] == md5($cipher);
-$sha1 = $_GET['sha1'] == sha1($cipher);
-$xor = $_GET['xor'] == xor($cipher,$hex);
-<?php
-
+<include 'flag.php'>
 function xor_string($string, $key){
     for($i = 0; $i < strlen($string); $i++)
         $string[$i] = ($string[$i] ^ $key[$i % strlen($key)]);
     return $string;
 }
 try{
-$success = 0;
 $cipher = "WebTF{}";
 $ascii = isset($_GET['ascii']) == iconv('UTF-8','ASCII',$cipher);
+if($ascii==1)
+    echo $part1;
 $hex = isset($_GET['hex']) == bin2hex($cipher);
+if($hex==1)
+    echo $part2;
 $rot13 = isset($_GET['rot13']) == str_rot13($cipher);
+if($rot13==1)
+    echo $part3;
 $base64 = isset($_GET['base64']) == base64_encode($cipher);
+if($base64==1)
+    echo $part4;
 $md5 = isset($_GET['md5']) == md5($cipher);
+if($md5==1)
+    echo $part5;
 $sha1 = isset($_GET['sha1']) == sha1($cipher);
+if($sha1==1)
+    echo $part6;
 $xor = isset($_GET['xor']) == xor_string($cipher,"c1ph3r");
+if($xor==1)
+    echo $part7;
+}
+catch(Exception $e){echo $e;}
+<?php
+<include 'flag.php'>
+function xor_string($string, $key){
+    for($i = 0; $i < strlen($string); $i++)
+        $string[$i] = ($string[$i] ^ $key[$i % strlen($key)]);
+    return $string;
+}
+try{
+$cipher = "WebTF{}";
+$ascii = isset($_GET['ascii']) == iconv('UTF-8','ASCII',$cipher);
+if($ascii==1)
+    echo $part1;
+$hex = isset($_GET['hex']) == bin2hex($cipher);
+if($hex==1)
+    echo $part2;
+$rot13 = isset($_GET['rot13']) == str_rot13($cipher);
+if($rot13==1)
+    echo $part3;
+$base64 = isset($_GET['base64']) == base64_encode($cipher);
+if($base64==1)
+    echo $part4;
+$md5 = isset($_GET['md5']) == md5($cipher);
+if($md5==1)
+    echo $part5;
+$sha1 = isset($_GET['sha1']) == sha1($cipher);
+if($sha1==1)
+    echo $part6;
+$xor = isset($_GET['xor']) == xor_string($cipher,"c1ph3r");
+if($xor==1)
+    echo $part7;
 }
 catch(Exception $e){echo $e;}
 ?>
